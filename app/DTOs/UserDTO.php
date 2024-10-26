7dd95136d125c97478c8e5de4f007daf345a28e6
Umid1982 <umidurinov14@gmail.com>
2024-10-26 08:52:46 +0500

    create project

---
+++ app/DTOs/UserDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DTOs;
+
+class UserDTO
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public string $password,
+        public string $gender,
+    )
+    {
+    }
+}
