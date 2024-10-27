7f51e3e22380fd5e6c203b8fb703bc53838ae31c
Umid1982 <umidurinov14@gmail.com>
2024-10-27 19:42:54 +0500

    Fix profile user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,14 +15,8 @@ class UserController extends Controller
 
     public function show(Request $request)
     {
-        $user = $request->user();
-
-        if ($user) {
-            return UserResource::make($user);
-        }
-        return response([
-            'message' => "No such user,something went wrong, check Logs!",
-            'success' => false
-        ]);
+        return $request->user()
+            ? UserResource::make($request->user())
+            : response(['message' => "Unauthenticated."],401);
     }
 }
